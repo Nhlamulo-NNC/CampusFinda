4af78e00ea793ee7db49f6c19eb6db4b2121c174
Nhlamulo-NNC <nncchauke573@gmail.com>
2025-12-14 15:30:08 +0100

    Delete campus-findaloginbackend directory

--- campus-findaloginbackend/src/main/java/com/repository/DeviceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.repository;
-
-import com.model.Device;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DeviceRepository extends JpaRepository<Device, Long> {}
-
--- campus-findaloginbackend/src/main/java/com/repository/MessageRepository.java
+++
@@ -1,6 +0,0 @@
-package com.repository;
-
-import com.model.Message;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MessageRepository extends JpaRepository<Message, Long> {}
